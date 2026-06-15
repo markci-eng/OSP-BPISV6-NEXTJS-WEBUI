7463ef1069ffef6c7243789457ac7db4d42a6a91
markci-eng <markci@stpeter.com.ph>
2026-06-15 15:51:10 +0800

    Update root-layout-client.tsx

--- app/root-layout-client.tsx
+++ app/root-layout-client.tsx
@@ -11,21 +11,21 @@ export default function RootLayoutClient({
 }) {
   const [queryClient] = useState(() => new QueryClient());
 
-  React.useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/service-worker.js")
-        .then((registration) => {
-          console.log(
-            "Service Worker registered with scope:",
-            registration.scope,
-          );
-        })
-        .catch((error) => {
-          console.error("Service Worker registration failed:", error);
-        });
-    }
-  }, []);
+  // React.useEffect(() => {
+  //   if ("serviceWorker" in navigator) {
+  //     navigator.serviceWorker
+  //       .register("/service-worker.js")
+  //       .then((registration) => {
+  //         console.log(
+  //           "Service Worker registered with scope:",
+  //           registration.scope,
+  //         );
+  //       })
+  //       .catch((error) => {
+  //         console.error("Service Worker registration failed:", error);
+  //       });
+  //   }
+  // }, []);
 
   return (
     <ColorModeProvider>
